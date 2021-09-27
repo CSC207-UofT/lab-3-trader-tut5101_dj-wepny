2ab364fa4f44462ef6b4c18f6b62ddce863041b9
yinid <enid.xiong@mail.utoronto.ca>
2021-09-27 19:34:52 -0400

    Fixed some error with documentations

--- src/main/java/Sheep.java
+++ src/main/java/Sheep.java
@@ -1,6 +1,11 @@
 public class Sheep implements Domesticatable, Tradable{
-    /* A Sheep class that implements the interface Domesticable and Drivable
+    /* A Sheep class that implements the interface Domesticable and Tradable
      */
+        private int price = 17;
+
+        public Sheep() {
+            this.price = 17;
+        }
         @Override
         public String sound() {
             return "Mehhhhh!";
@@ -8,6 +13,6 @@ public class Sheep implements Domesticatable, Tradable{
 
         @Override
         public int getPrice() {
-            return 17;
+            return this.price;
         }
 }
