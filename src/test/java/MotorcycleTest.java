e84220162ef451270c443f47428aaedbe3e15c9d
jennylin331431 <jennylin331431@gmail.com>
2021-09-27 19:21:28 -0400

    doneee

---
+++ src/test/java/MotorcycleTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MotorcycleTest {
+    Motorcycle motorcycle;
+
+    @Before
+    public void setUp() throws Exception {
+        motorcycle = new Motorcycle();
+    }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(100, motorcycle.getPrice());}
+
+    @Test(timeout = 50)
+    public void TestGetMaxSpeed() {
+        assertEquals(1, motorcycle.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestUpgradeSpeed() {
+        motorcycle.upgradeSpeed();
+        assertEquals(125, motorcycle.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestDowngradeSpeed() {
+        motorcycle.downgradeSpeed();
+        assertEquals(75, motorcycle.getMaxSpeed());
+    }
+}
+
